4450df938143e05d64761f695373660bbeaf4684
Jenny Paul <>
2020-11-18 19:32:16 -0600

    button functionality to start lander

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-//Initial starting location of mars lander; 
+//Initial starting location of mars lander;
 //gravity, engine set points and heatup are constant
 const initialAltitude = 240000;
 const initialSpeed = 4600;
@@ -6,8 +6,8 @@ const gravity = 3.7;
 const engHeatRate = 20;
 const engHeatSet = 1500;
 
-//initial 'dummy lights' showing at simulation start; 
-//engine warmup light is off, parachute is on, GCS is off, 
+//initial 'dummy lights' showing at simulation start;
+//engine warmup light is off, parachute is on, GCS is off,
 //landing arm is off, and thrusters are off
 let engWarmUp = false;
 let parachute = true;
@@ -15,28 +15,27 @@ let GCS = false;
 let landArm = false;
 let thrusters = false;
 
-//Two gauges showing Engine Temperature and Thrust; 
+//Two gauges showing Engine Temperature and Thrust;
 //drag is from parachute and removed when parachute is dropped
 let engCurrentTemp = 0;
 let drag = 45;
 let thrust = 0;
 
-//setting Speed and Altitude to the starting values when 
+//setting Speed and Altitude to the starting values when
 //entering mars atmosphere
 let currentSpeed = initialSpeed;
 let currentAltitude = initialAltitude;
 
-//Timing control runs until broken by clearInterval; 
+//Timing control runs until broken by clearInterval;
 //engineControl function is repeated every 1000 ms (1 second)
 let i = setInterval(engineControl, 1000);
 
-//Counter is 'Mission Time' and counts in seconds 
+//Counter is 'Mission Time' and counts in seconds
 //(e.g. {exempli gratia} 1 second since descent starts, 2 seconds since descent starts, etc.)
-let counter = 0;
-
 //controls the entire entry, descrent, GCS control and eventually landing of the mars lander
 function engineControl(){
-    
+
+  let counter = 0;
     //Phase 3 - Parachute released and terminal descent start
     if (engCurrentTemp === engHeatSet){
         currentSpeed = currentSpeed + gravity - thrust;
@@ -44,14 +43,14 @@ function engineControl(){
         GCS = true;
         engWarmUp = false;
     }
-    //Phase 1 - Parachute Descent; immediately transition to 
+    //Phase 1 - Parachute Descent; immediately transition to
     //Phase 2 - Engine Warmup
     else {
         currentSpeed = currentSpeed + gravity - thrust - drag;
         engCurrentTemp = engCurrentTemp + engHeatRate;
         engWarmUp = true;
     }
-    
+
     //Phase 5 - Touchdown
     if (currentAltitude <= 0){
         currentSpeed = 0;
@@ -67,14 +66,14 @@ function engineControl(){
     else {
         currentAltitude = currentAltitude - currentSpeed;
     }
-    
+
     //Phase 3 - Parachute released and terminal descent start
-    // %100 Thrust during Phase 3 until terminal descent is arrested 
+    // %100 Thrust during Phase 3 until terminal descent is arrested
     if (currentSpeed > 150 && GCS === true){
         thrust = 150;
         thrusters = true;
     }
-    
+
     //Phase 4 - Drop and engine on/off
     // %50 Thrust while engine 'on' combination to approach ground slowly
     else if (currentSpeed > 10 && GCS === true){
@@ -82,25 +81,25 @@ function engineControl(){
         thrusters = true;
     }
     //Phase 4 - Drop and engine on/off
-    //The 'drop' part where thrusters are 'off' to conserve fuel and get to ground faster 
+    //The 'drop' part where thrusters are 'off' to conserve fuel and get to ground faster
     else {
         thrust = 0;
         thrusters = false;
     }
-    
+
     //Increments Mission Timer by 1 second
     counter++;
     //Displays in the console (for code testing) status of monitored systems
-    console.log(Math.round(counter) + ' seconds.');
-    console.log(Math.round(currentSpeed) + ' meters/second.');
-    console.log(Math.round(currentAltitude) + ' meters.');
-    console.log(Math.round(engCurrentTemp) + ' K.');
-    console.log('Parachute attached? ' + parachute);
-    console.log('Engine Warmup Cycle? ' + engWarmUp);
-    console.log('GCS Enabled? ' + GCS);
-    console.log('Thrusters active? ' + thrusters);
-    console.log('Landing arm contact? ' + landArm);
+    document.write(Math.round(counter) + ' seconds.');
+    document.write(Math.round(currentSpeed) + ' meters/second.');
+    document.write(Math.round(currentAltitude) + ' meters.');
+    document.write(Math.round(engCurrentTemp) + ' K.');
+    document.write('Parachute attached? ' + parachute);
+    document.write('Engine Warmup Cycle? ' + engWarmUp);
+    document.write('GCS Enabled? ' + GCS);
+    document.write('Thrusters active? ' + thrusters);
+    document.write('Landing arm contact? ' + landArm);
 }
 
 //Mission ends with lander coming to a stop on surface of mars
-console.log('Landing Success!');
+document.write('Landing Success!');
